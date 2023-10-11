b6fec5ee1f10c9f2c4bce0a3009cabb9b6f9f58d
lajta <svetlupand@gmail.com>
2023-10-11 17:42:14 +0400

    added all files to local repos

---
+++ js/testsait.js
@@ -0,0 +1,15 @@
+/**/
+// document.write("hello");
+
+
+let elem = document.querySelector("[data-widget-name]");
+
+let attr = elem.getAttribute("data-widget-name");
+
+// let attr = elem.attributes();
+
+console.log(attr);
+
+
+// console.log(elem.dataset.widgetName);
+
